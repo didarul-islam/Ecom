4723297f8a6d871e3241477f0157c152c186269c
Didarul Islam <71816650+didarul-islam@users.noreply.github.com>
2021-03-12 19:28:55 +0600

    Add

---
+++ admin/blog/storage/framework/views/d65780d9829509a8deda2feca0fa066698500116.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Ecommarce site</title>
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+
+</head>
+<body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<div id="root">
+
+</div>
+
+<script src="<?php echo e(asset('js/app.js')); ?>"></script>
+
+</body>
+</html>
+<?php /**PATH C:\Users\SANEEM\Desktop\Ecom\admin\blog\resources\views/index.blade.php ENDPATH**/ ?>
